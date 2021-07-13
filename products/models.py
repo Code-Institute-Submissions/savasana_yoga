c30eeef6ba7da283863352cc5fd5b86d2dad20f0
TomC2311 <tomaslcripps@gmail.com>
2021-07-13 10:34:33 +0000

    Customize admin database to display verbose name and correct layout

--- products/models.py
+++ products/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=250)
     friendly_name = models.CharField(max_length=250, null=True, blank=True)
 
@@ -16,8 +19,9 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=250)
+    sku = models.CharField(max_length=250, null=True, blank=True)
     description = models.TextField()
-    location = models.CharField(max_length=250)
+    location = models.CharField(max_length=250, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
