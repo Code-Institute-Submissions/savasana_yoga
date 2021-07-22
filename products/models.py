a23646f80d97bfb2dc3f88bf9e0d1cb3e7025380
TomC2311 <tomaslcripps@gmail.com>
2021-07-22 09:39:30 +0000

    Remove static ratings from product models

--- products/models.py
+++ products/models.py
@@ -25,7 +25,6 @@ class Product(models.Model):
     information = models.CharField(max_length=250, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     number_of_sessions = models.IntegerField(validators=[MaxValueValidator(50), MinValueValidator(1)], default=10)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
