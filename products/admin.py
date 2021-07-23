e139a8815d15695fec95f6dff9501dbacd5c407a
TomC2311 <tomaslcripps@gmail.com>
2021-07-23 09:15:28 +0000

    Create difficulty class and update product and category model

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, DifficultyLevel
 
 # Register your models here.
 
@@ -10,6 +10,7 @@ class ProductAdmin(admin.ModelAdmin):
         'category',
         'price',
         'image',
+        'difficulty',
     )
 
     ordering = ('name',)
@@ -22,5 +23,12 @@ class CategoryAdmin(admin.ModelAdmin):
     )
 
 
+class DifficultyLevelAdmin(admin.ModelAdmin):
+    list_display = (
+        'difficulty_level',
+    )
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(DifficultyLevel, DifficultyLevelAdmin)
--- products/models.py
+++ products/models.py
@@ -17,12 +17,28 @@ class Category(models.Model):
         return self.friendly_name
 
 
+class DifficultyLevel(models.Model):
+    class Meta:
+        verbose_name_plural = 'Difficulty'
+
+    BEGINNER = 'Beginner'
+    ADVANCED = 'Advanced'
+    DIFFICULTY_LEVEL_CHOICES = [
+        (BEGINNER, 'Beginner'),
+        (ADVANCED, 'Advanced')
+    ]
+    difficulty_level = models.CharField(max_length=25, choices=DIFFICULTY_LEVEL_CHOICES, default=BEGINNER)
+
+    def __str__(self):
+        return self.difficulty_level
+
+
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=250)
     description = models.TextField()
     tagline = models.CharField(max_length=250, null=True, blank=True)
-    information = models.CharField(max_length=250, null=True, blank=True)
+    difficulty = models.ForeignKey('DifficultyLevel', null=True, blank=True, on_delete=models.SET_NULL)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     number_of_sessions = models.IntegerField(validators=[MaxValueValidator(50), MinValueValidator(1)], default=10)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
