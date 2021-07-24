0902ca9d58728ab5bd4c4c4bf6c03c64aeb4f8f5
TomC2311 <tomaslcripps@gmail.com>
2021-07-24 12:31:30 +0000

    Create timetable model to display yoga class, days and times

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import TimeTable
+
+# Register your models here.
+
+
+class TimeTableAdmin(admin.ModelAdmin):
+    list_display = (
+        'day',
+        'product',
+        'time',
+    )
+
+    ordering = ('product',)
+
+
+admin.site.register(TimeTable, TimeTableAdmin)
--- timetable/models.py
+++ timetable/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+from django.conf import settings
+
+from products.models import Product
+
 # Create your models here.
+
+class TimeTable(models.Model):
+
+    MON = 'Monday'
+    TUE = 'Tuesday'
+    WED = 'Wednesday'
+    THU = 'Thursday'
+    FRI = 'Friday'
+    SAT = 'Saturday'
+    SUN = 'Sunday'
+
+    DAYS_OF_THE_WEEK = [
+        ('MON', 'Monday'),
+        ('TUE', 'Tuesday'),
+        ('WED', 'Wednesday'),
+        ('THU', 'Thursday'),
+        ('FRI', 'Friday'),
+        ('SAT', 'Saturday'),
+        ('SUN', 'Sunday')
+    ]
+
+    day = models.CharField(max_length=10, choices=DAYS_OF_THE_WEEK, default=MON)
+    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.SET_NULL)
+    time = models.TimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.day
