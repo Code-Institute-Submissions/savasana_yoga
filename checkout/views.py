b544f518c1e0f4fa08787af71d35e6dda5306fc0
TomC2311 <tomaslcripps@gmail.com>
2021-08-01 10:10:58 +0000

    Implement stripe elements for checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IyYoJL6RjrtUtADyEA0Zlg8bh3P7ozp3BQYRsAaf540gdjEelJIEXG6yKum7qxk9xcLFMGeTfxmiGlCVkUpqG05001qBPXBqW',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
