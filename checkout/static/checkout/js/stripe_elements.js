c433bbcf948ceb33ab7ee885b4c1a92df051aeeb
TomC2311 <tomaslcripps@gmail.com>
2021-08-01 11:43:02 +0000

    Create functionality to handle validation errors for checkout cards

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,8 +1,11 @@
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
+
     CSS from here: 
     https://stripe.com/docs/stripe-js
+
+    JS code from Code Institute Boutique Ado Project
 */
 
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);  /* Slice removes quoatation marks */
@@ -26,3 +29,19 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
