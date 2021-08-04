0e8f4d171da90a2e55d12e11fe9044f00c22eb57
TomC2311 <tomaslcripps@gmail.com>
2021-08-04 13:01:27 +0000

    Implement webhook handler functionality to store cart information to prevent double orders and handling of payment processing errors

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -63,8 +63,8 @@ form.addEventListener('submit', function(ev) {  // prevents form from being subm
     };
     var url = '/checkout/cache_checkout_data/';
 
-    $.post(url, postData).done(function () {  // posts data to cache_checkout_data view
-        stripe.confirmCardPayment(clientSecret, {  // calls confirm payment intent from stripe 
+    $.post(url, postData).done(function () {
+        stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: card,
                 billing_details: {
@@ -78,6 +78,18 @@ form.addEventListener('submit', function(ev) {  // prevents form from being subm
                         country: $.trim(form.country.value),
                         state: $.trim(form.county.value),
                     }
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
                 }
             },
         }).then(function(result) { 
