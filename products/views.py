79925ccdfb55a73a8b30aee222cfa18fc57866d3
TomC2311 <tomaslcripps@gmail.com>
2021-08-15 17:16:52 +0000

    Implement delete product functionality

--- products/views.py
+++ products/views.py
@@ -77,11 +77,11 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Successfully added yoga class!')
-            return redirect(reverse('add_product'))
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_info', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add yoga class. Please ensure the form is valid.')
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
      
@@ -100,10 +100,10 @@ def edit_product(request, product_id):
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Successfully updated yoga class!')
-            return redirect(reverse('product_detail', args=[product.id]))
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_info', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update yoga class. Please ensure the form is valid.')
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
@@ -115,6 +115,15 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
+
 
 def view_timetable(request,):
     """ A view to renders the timetable page """
