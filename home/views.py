3ca5ff7c6f259ab726540f0366cee7bf0a173db7
TomC2311 <tomaslcripps@gmail.com>
2021-08-21 08:37:59 +0000

    Add latest blog post to home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+    posts = Post.objects.order_by('-created_on')[0:1]
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'home/index.html', context)
+
