80a5d05183f7786e4d6df6631ef028c376bb3779
TomC2311 <tomaslcripps@gmail.com>
2021-08-27 11:44:34 +0000

    Add about section to home page and implement dynamic product cards to home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from products.models import Product, Category
 
 # Create your views here.
 
@@ -7,9 +8,10 @@ from blog.models import Post
 def index(request):
     """ A view to return the index page """
     posts = Post.objects.order_by('-created_on')[0:1]
-
+    products = Product.objects.filter(category__name='beginner_class')[0:3]
     context = {
         'posts': posts,
+        'products': products,
     }
 
     return render(request, 'home/index.html', context)
