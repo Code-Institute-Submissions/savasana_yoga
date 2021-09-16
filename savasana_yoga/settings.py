b200046ce98579a6d8a5b9b9969739b9862529a1
TomC2311 <tomaslcripps@gmail.com>
2021-09-16 13:30:16 +0000

    Remove Secret key from settings and set development debug

--- savasana_yoga/settings.py
+++ savasana_yoga/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4tb427)iez5yt649ov9vm%l05+1+-1lcmf!s&2vvam*3%@@6!0'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['tomc-savasana-yoga.herokuapp.com', 'localhost']
 
