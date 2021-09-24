9cd7ba55086741551bd09612ac57b0f97d4f347c
TomC2311 <tomaslcripps@gmail.com>
2021-09-24 12:50:14 +0000

    Update blog and profile toasts, and move blog comment form to new html file

--- blog/views.py
+++ blog/views.py
@@ -31,9 +31,27 @@ def blog_post_detail(request, blog_post_id):
 
     new_comment = None
 
+    context = {
+        'blog_post': blog_post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'awaiting_approval_comments': awaiting_approval_comments,
+    }
+
+    return render(request, 'blog/blog_post_detail.html', context)
+
+
+@login_required
+def add_blog_post_comment(request, blog_post_id):
+    """
+    A view to allow users to add a comment to a blog
+    """
+
+    blog_post = get_object_or_404(Post, pk=blog_post_id)
+
     # Comment posted
     if request.method == 'POST':
-        comment_form = CommentForm(data=request.POST)
+        comment_form = CommentForm(request.POST)
 
         if comment_form.is_valid():
             # Create Comment object but don't save to database yet
@@ -44,21 +62,21 @@ def blog_post_detail(request, blog_post_id):
             new_comment.author = request.user
             # Save the comment to the database
             new_comment.save()
-            messages.success(request, 'Your comment is awaiting moderation!')
-            comment_form = CommentForm()
+            messages.info(request, 'Your comment is awaiting moderation!')
             return redirect(reverse('blog_post_detail', args=[blog_post.id]))
+
+        else: messages.error(request,'Something went wrong. Please try again.')
+
     else:
         comment_form = CommentForm()
 
+    template = 'blog/add_blog_comment.html'
     context = {
-        'blog_post': blog_post,
-        'comments': comments,
-        'new_comment': new_comment,
         'comment_form': comment_form,
-        'awaiting_approval_comments': awaiting_approval_comments,
+        'blog_post': blog_post,
     }
 
-    return render(request, 'blog/blog_post_detail.html', context)
+    return render(request, template, context)
 
 
 @login_required
@@ -75,7 +93,7 @@ def add_blog_post(request):
             blog_post = form.save(commit=False)
             blog_post.author = request.user
             blog_post = form.save()
-            messages.success(request, 'Successfully added post!')
+            messages.info(request, 'Successfully added post!')
             return redirect(reverse('blog_post_detail', args=[blog_post.id]))
         else:
             messages.error(request, 'Failed to add post. Please ensure the form is valid.')
@@ -105,7 +123,7 @@ def edit_blog_post(request, blog_post_id):
         form = PostForm(request.POST, request.FILES, instance=blog_post)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Successfully updated blog post!')
+            messages.info(request, 'Successfully updated blog post!')
             return redirect(reverse('blog_post_detail', args=[blog_post.id]))
         else:
             messages.error(
@@ -134,7 +152,7 @@ def delete_blog_post(request, blog_post_id):
 
     if request.user == blog_post.author or request.user.is_superuser:
         blog_post.delete()
-        messages.success(request, 'Blog Post deleted!')
+        messages.info(request, 'Blog Post deleted!')
         return redirect(reverse('view_blog'))
 
     else:
@@ -153,7 +171,7 @@ def comment_approve(request, comment_id):
 
     if request.user.is_superuser:
         comment.approve()
-        messages.success(request, 'Comment has been approved!')
+        messages.info(request, 'Comment has been approved!')
         return redirect(reverse('view_blog'))
 
     else:
@@ -172,7 +190,7 @@ def comment_remove(request, comment_id):
 
     if request.user == comment.author or request.user.is_superuser:
         comment.delete()
-        messages.success(request, 'Your comment is deleted !')
+        messages.info(request, 'Comment has been deleted !')
         return redirect(reverse('view_blog'))
 
     else:
@@ -194,9 +212,9 @@ def edit_comment(request, comment_id):
             form = CommentForm(request.POST, instance=comment)
             if form.is_valid():
                 form.save()
-                messages.success(
+                messages.info(
                             request,
-                            'Your comment has been successfully edited')
+                            'Comment has been successfully edited')
                 return redirect(reverse('view_blog'))
             else:
                 messages.error(request, 'Unable to edit this comment. Please ensure the form is valid.')
