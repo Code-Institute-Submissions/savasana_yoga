195e73f8f45d5b325ce8cdb0458c3693e9d25ef1
TomC2311 <tomaslcripps@gmail.com>
2021-10-01 13:46:38 +0000

    implement cleaned_data function on contact form

--- contact/views.py
+++ contact/views.py
@@ -10,27 +10,29 @@ from .forms import ContactForm
 
 
 def contact(request):
-   
+
     if request.method == 'POST':
         contact_form = ContactForm(request.POST)
         user_message = request.POST.get('message')
         subject = request.POST.get('subject')
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
         user_email = request.POST.get('user_email')
 
         if contact_form.is_valid():
-
+            user_email = contact_form.cleaned_data['email_address']
+            subject = contact_form.cleaned_data['subject']
+            body = render_to_string(
+                'contact/emails/email_received_body.txt',
+                {'contact_email': settings.DEFAULT_FROM_EMAIL})
             send_mail(
                 subject,
-                user_message,
+                body,
                 settings.DEFAULT_FROM_EMAIL,
                 [user_email]
             )
-   
+
             contact_form.save()
-            messages.info(request, 'Message sent successfully !')
-            return redirect(reverse('contact_success'))
+            messages.success(request, 'Message sent successfully !')
+            return redirect(reverse('contact'))
 
         else:
             messages.error(request, 'There was a problem with your message. Please try again.')
